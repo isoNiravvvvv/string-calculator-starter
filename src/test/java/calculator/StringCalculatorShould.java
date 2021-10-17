0a969a6acd31317ede366edbe5a2f3e29916e136
Nirav Mithari <nirav.mithari@gmail.com>
2021-10-17 23:50:37 +0530

    Code completed

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -6,15 +6,59 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
 
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
-    }
+	public static void main(String args[]) {
+	      org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
+	    }
 
-    @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
-    }
+	   @Test
+	   public void testEmptyString() {
+	       assertEquals(0, StringCalculator.add(""));
+	   }
+
+	   @Test
+	   public void testOneNumber() {
+	       assertEquals(1, StringCalculator.add("1"));
+	   }
+
+	   @Test
+	   public void testTwoNumbers(){
+	       assertEquals(3, StringCalculator.add("1,2"));
+	   }
+
+	   @Test
+	    public void testThreeNumbers(){
+	       assertEquals(6, StringCalculator.add("1,2,3"));
+	    }
+
+	    @Test
+	    public void testNewLine(){
+	       assertEquals(6, StringCalculator.add("1\n2,3"));
+	    }
+
+	    @Test
+	    public void testNegativeNumver(){
+	       try {
+	           StringCalculator.add("-1,2");
+	       }
+	       catch (IllegalArgumentException e){
+	           assertEquals(e.getMessage(), "Negatives not allowed: -1");
+	       }
+
+	       try {
+	           StringCalculator.add("2,-4,3,-5");
+	       }
+	       catch (IllegalArgumentException e){
+	           assertEquals(e.getMessage(), "Negatives not allowed: -4,-5");
+	       }
+	    }
+
+	    @Test
+	    public void testOverThousand(){
+	       assertEquals(2, StringCalculator.add("1000,2"));
+	    }
+
+	    @Test
+	    public void testOtherDelimiter(){
+	       assertEquals(3, StringCalculator.add("//;\n1;2"));
+	    }
 }
